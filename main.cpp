742c4b2815b499ffad02a58268aae9877c2a4857
Maxim Sheshukov <sheshukovmaxim@gmail.com>
2017-09-12 21:14:30 +0700

    PubSub v.0.2

--- main.cpp
+++ main.cpp
@@ -1,18 +1,22 @@
 #include <typeinfo>
 #include <iostream>
-#include <map>
 #include <cassert>
+#include <deque>
 
 template<typename MSG> class Dispatcher;
 template<typename MSG> class Publisher;
-template<typename MSG> class BaseSubscriber
+
+template<typename MSG>
+class BaseSubscriber
 {
     friend class Dispatcher<MSG>;
 protected:
+    BaseSubscriber()
+    {
+        Dispatcher<MSG>::get().registerSubscriber(this);
+    }
     virtual void notify(const MSG &) = 0;
 };
-
-class Message {};
 
 template<typename MSG>
 class Dispatcher
@@ -30,30 +34,21 @@ public:
     }
     void registerSubscriber(BaseSubscriber<MSG> * sub)
     {
-        const std::string name = typeid(MSG).name();
-        m_msg2sub.insert(std::make_pair(name, sub));
+        m_subs.push_back(sub);
     }
     void sendMessage(const MSG & msg)
     {
-        const std::string name = typeid(msg).name();
-        if (auto count = m_msg2sub.count(name))
-        {
-            auto it = m_msg2sub.find(name);
-            for (size_t i = 0; i < count; ++i, ++it)
-                it->second->notify(msg);
-        }
+        for (auto sub: m_subs)
+            sub->notify(msg);
     }
 
 private:
-   std::multimap<std::string, BaseSubscriber<MSG> *> m_msg2sub;
+   std::deque<BaseSubscriber<MSG>*> m_subs;
 };
 
 template<typename MSG>
 class Publisher
 {
-public:
-    Publisher() {}
-
 public:
     void publish(const MSG & msg)
     {
@@ -61,27 +56,38 @@ public:
     }
 };
 
-template<typename MSG>
-class Subscriber : protected BaseSubscriber<MSG>
+template<typename MSG, typename... ARGS>
+class Subscriber
+    : protected Subscriber<ARGS...>
+    , protected BaseSubscriber<MSG>
 {
-protected:
-    Subscriber()
-        { Dispatcher<MSG>::get().registerSubscriber(this); }
-    virtual void notify(const MSG &) = 0;
 };
 
-struct MyMessage : Message
+template<typename MSG>
+class Subscriber<MSG> : protected BaseSubscriber<MSG> {};
+
+struct MyMessage
 {
     MyMessage(int val) : val(val) {}
     int val = 0;
 };
 
-class MySub : protected Subscriber<MyMessage>
+struct MyRequest
+{
+    MyRequest(int val) : val(val) {}
+    int val = 0;
+};
+
+class MySub : protected Subscriber<MyMessage, MyRequest>
 {
 protected:
     void notify(const MyMessage & msg) override
     {
-        std::cout << msg.val << std::endl;
+        std::cout << "Message: " << msg.val << std::endl;
+    }
+    void notify(const MyRequest & r) override
+    {
+        std::cout << "Request: " << r.val << std::endl;
     }
 };
 
@@ -89,5 +95,6 @@ int main(int argc, char *argv[])
 {
     MySub sub;
     Publisher<MyMessage>().publish(MyMessage(42));
+    Publisher<MyMessage>().publish(MyMessage(99));
     return 0;
 }
